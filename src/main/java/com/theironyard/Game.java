7fc3a1a49f8fe903e5e35ca4883db1f308ddbd2d
carolineevail <carolineevail@gmail.com>
2016-03-09 10:09:40 -0500

    Add genre

--- src/main/java/com/theironyard/Game.java
+++ src/main/java/com/theironyard/Game.java
@@ -1,8 +1,10 @@
 package com.theironyard;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by Caroline on 3/8/16.
@@ -13,16 +15,24 @@ public class Game {
     @GeneratedValue
     int id;
 
+    @Column(nullable = false)
     String name;
+
+    @Column(nullable = false)
     String platform;
+
+    @Column(nullable = false)
+    String genre;
+
     int releaseYear;
 
     public Game() {
     }
 
-    public Game(String name, String platform, int releaseYear) {
+    public Game(String name, String platform, String genre, int releaseYear) {
         this.name = name;
         this.platform = platform;
+        this.genre = genre;
         this.releaseYear = releaseYear;
     }
 }
